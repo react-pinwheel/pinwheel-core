6f4171cb8f351f09dddec281f08153535466bb56
Riccardo Tartaglia <tartaglia.riccardo@gmail.com>
2021-02-22 21:44:12 +0100

    up

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,45 @@
 import pkg from "./package.json";
 import babel from "@rollup/plugin-babel";
 import external from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import del from "rollup-plugin-delete";
+
+const EXTENSIONS = [".ts", ".tsx", ".js", ".jsx", ".json"];
 
 export default {
   input: pkg.source,
   output: [
-    { file: pkg.main, format: "cjs" },
-    { file: pkg.module, format: "esm" },
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named",
+    },
+    {
+      file: pkg.module,
+      format: "esm",
+    },
   ],
   plugins: [
-    external(),
+    external({
+      includeDependencies: true,
+    }),
     babel({
-      exclude: "node_modules/**",
+      babelrc: false,
+      extensions: EXTENSIONS,
+      presets: [
+        ["@babel/preset-env", { modules: false }],
+        "@babel/preset-react",
+      ],
+      exclude: ["node_modules/**"],
+    }),
+    commonjs({
+      include: /node_modules/,
+    }),
+    resolve({
+      extensions: EXTENSIONS,
+      preferBuiltins: false,
     }),
     del({ targets: ["dist/*"] }),
   ],
-  external: Object.keys(pkg.peerDependencies || {}),
 };
